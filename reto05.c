f7f14a90d0a7e354d7b598ce26f94df0455d101c
JuanDaza <dazacarojuandavid@gmail.com>
2025-08-21 19:44:21 -0500

    Comentarios reto5

--- reto05.c
+++ reto05.c
@@ -15,24 +15,22 @@
 
 
 int main(){
-	int *ptr, i, n1, n2;
-	printf("Enter size: ");
-	scanf("%d", &n1);
-	ptr = (int*) malloc(n1 * sizeof(int));
-	printf("Addresses of previously allocated memory:");
+	int *ptr, i, n1, n2; // crea un puntero a int, y 3 variables int
+	printf("Enter size: "); // pide al usuario ingresar el tamaño
+	scanf("%d", &n1); // lee la entrada del usuario
+	ptr = (int*) malloc(n1 * sizeof(int)); // asigna memoria para n1 elementos de tipo int
+	printf("Addresses of previously allocated memory:"); // imprime las direcciones de la memoria previamente asignada
 	for (i = 0; i <n1; i++)
-		printf("\n\n p = %p\n", ptr+i);
-	printf("\n Enter the new size: ");
-	scanf("%d", &n2);
-	// rellocating the memory
-	ptr = realloc(ptr, n2 * sizeof(int));
-	printf("Addresses of newly allocated memory: ");
+		printf("\n\n p = %p\n", ptr+i); // imprime la dirección de cada elemento
+	printf("\n Enter the new size: "); // pide ingresar un nuevo tamaño 
+	scanf("%d", &n2); // almacena el numero en n2
+	ptr = realloc(ptr, n2 * sizeof(int));  // realoca la memoria, puede aumentar el tamaño y reducirlo y perder la informacion que no entre en ese tamaño
+	printf("Addresses of newly allocated memory: "); // muestra las direcciones de la nueva memoria asignada)
 	for (i = 0; i < n2; i++)
 		printf("\n\n p = %p\n", ptr+i);
 
-	free(ptr);
+	free(ptr); // libera la memoria asignada
 
-
-	return 0;
+	return 0; // termina el programa
 }
 
